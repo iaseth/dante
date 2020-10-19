0dfec6c3d5079b75d87b616f5591052a2fc40b80
Ankur Seth <iankurseth@gmail.com>
2020-10-20 03:36:19 +0530

    Implemented basic dante_new_quote() and dante_delete_quote()

--- lib/dante_quote.c
+++ lib/dante_quote.c
@@ -1,5 +1,15 @@
 #include "dante_quote.h"
 
+#include <stdlib.h>
 
+DanteQuote dante_new_quote (Dante dante) {
+	DanteQuote quote = malloc(sizeof(DanteQuote_t));
+	return quote;
+}
+
+DanteQuote dante_delete_quote (Dante dante, DanteQuote quote) {
+	free(quote);
+	return NULL;
+}
 
 
--- src/dante.c
+++ src/dante.c
@@ -4,5 +4,7 @@
 
 int main (int argc, char const *argv[]) {
 	Dante dante = dante_new_dante();
+	DanteQuote quote = dante_new_quote(dante);
+	quote = dante_delete_quote(dante, quote);
 	dante = dante_delete_dante(dante);
 }
