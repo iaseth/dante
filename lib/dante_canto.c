5bc888590cec71861e9ac4cd41984bcab003170c
Ankur Seth <iankurseth@gmail.com>
2020-10-20 13:12:01 +0530

    Implemented basic dante_new_canto() and dante_delete_canto()

--- lib/dante_canto.c
+++ lib/dante_canto.c
@@ -1,5 +1,19 @@
 #include "dante_canto.h"
 
+#include <stdlib.h>
 
+DanteCanto dante_new_canto (Dante dante) {
+	DanteCanto canto = malloc(sizeof(DanteCanto_t));
+	canto->quotes = NULL;
+	canto->quote_count = 0;
+	canto->quote_capacity = 0;
+	return canto;
+}
+
+DanteCanto dante_delete_canto (Dante dante, DanteCanto canto) {
+	free(canto->quotes);
+	free(canto);
+	return NULL;
+}
 
 
--- src/dante.c
+++ src/dante.c
@@ -5,13 +5,16 @@
 int main (int argc, char const *argv[]) {
 	Dante dante = dante_new_dante();
 	DanteQuote quote = dante_new_quote(dante, "Hello, Dante!");
+	DanteCanto canto = dante_new_canto(dante);
 
 	dante_print_quote_details(quote);
+	dante_print_canto_details(canto);
 	dante_print_dante_details(dante);
 
 	//printf("%s\n", *quote);
 	dante_print_struct_sizes();
 
 	quote = dante_delete_quote(dante, quote);
+	canto = dante_delete_canto(dante, canto);
 	dante = dante_delete_dante(dante);
 }
