bf834a8cb2f413372e2c0eb2ed8b3668aa53fff0
Ankur Seth <iankurseth@gmail.com>
2020-10-20 12:36:01 +0530

    Now printing directly as buffer is the first element

--- src/dante.c
+++ src/dante.c
@@ -6,7 +6,7 @@ int main (int argc, char const *argv[]) {
 	Dante dante = dante_new_dante();
 	DanteQuote quote = dante_new_quote(dante, "Hello, Dante!");
 
-	printf("%p == %p\n", quote->buffer, quote);
+	printf("%s\n", *quote);
 	dante_print_struct_sizes();
 
 	quote = dante_delete_quote(dante, quote);
