36197a19db90f78ca838c8a5342d287eed0fcc88
Ankur Seth <iankurseth@gmail.com>
2020-11-09 10:08:43 +0530

    Added string func declarations.

--- include/dante/string.h
+++ include/dante/string.h
@@ -3,6 +3,13 @@
 
 #include "dante/types.h"
 
+DanteString dante_new_string ();
+DanteString dante_new_string_s (char *source);
+DanteString dante_new_string_n (size_t length);
+DanteString dante_new_string_d (DanteString dsource);
+
+DanteString dante_delete_string (DanteString d);
+
 
 
 #endif
